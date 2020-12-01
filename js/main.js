e3c303b4f80fc810c7916a75c6510bf46ffa63da
Mike <miguelgc95@gmail.com>
2020-12-01 11:38:45 +0100

    functional modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function togforReminder(){
-    document.getElementById("reminderToHide").classList.toggle("hide-me")
-}
-
-document.getElementById("reminderCheck").addEventListener("click", togforReminder);
-
 function togForEndDate(){
     document.getElementById("endDateToHide").classList.toggle("hide-me")
 }
 
+function togforReminder(){
+    document.getElementById("reminderToHide").classList.toggle("hide-me")
+}
+
 document.getElementById("endDateCheck").addEventListener("click", togForEndDate);
+document.getElementById("reminderCheck").addEventListener("click", togforReminder);
